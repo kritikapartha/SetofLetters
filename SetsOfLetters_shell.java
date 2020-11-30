deac6ca00cc7d3ab6e70a04382f778283433c620
kritikaps11 <67285368+kritikaps11@users.noreply.github.com>
2020-11-30 13:43:43 -0500

    Add files via upload

---
+++ SetsOfLetters_shell.java
@@ -0,0 +1,91 @@
+//Name: Kritika Partha  
+//Date: March 08 2020
+// What I learned: I learned how to create hashsets and add elements to the hashset. I also got refreshed
+//about how to read a file and what exceptions to throw for file reading. 
+// Question:  If Java didn't implement Sets, how would you solve this programming problem?  Be creative!
+// I would use a priority queue and label characters with certain numbers, such that A is 1. When
+//items are added to the PQ, the compiler would organize the elements alphabetically according to priority. 
+import java.util.*;
+import java.io.*;
+public class Pd6ParthaKritikaSetofLetters_shell {
+   public static void main (String [] args) throws FileNotFoundException
+   {
+      ArrayList<HashSet> lowercase = new ArrayList <HashSet>();
+      ArrayList<HashSet> uppercase = new ArrayList <HashSet>();
+      ArrayList<HashSet> punctuation = new ArrayList <HashSet>();
+         File file = new File ("test.txt");
+         Scanner sc = new Scanner (file);
+         while (sc.hasNextLine())
+            {
+               HashSet<Character> lower = new HashSet <Character>();
+               HashSet<Character> upper = new HashSet <Character>();
+               HashSet<Character> punct = new HashSet <Character>();
+                  char[] arr = sc.nextLine().toCharArray();
+                     for (Character c:arr){
+                        if(Character.isLetter(c))
+                           {
+                              if(Character.isUpperCase(c))
+                                 upper.add(c);
+                              else
+                                 lower.add(c);
+                           }
+                         punct.add(c);     
+                     }
+               lowercase.add(lower);
+               uppercase.add(upper);
+               punctuation.add(punct);               
+            }
+            
+       TreeSet<Character> lowerOrg = new TreeSet <Character>();
+       TreeSet<Character> upperOrg = new TreeSet <Character>();
+       TreeSet<Character> punctOrg = new TreeSet <Character>();
+         for (Object x: lowercase.get(0))
+            {
+               boolean all = true;
+                  for (HashSet set:lowercase)
+                     {
+                        if(!(set.contains(a)))
+                           {
+                              all = false;
+                              break;
+                           }
+                     }
+            }
+         if(all)
+           {
+            lowerOrg.add((char)a);
+           }
+         for (Object a:punctuation.get(0))
+            {
+               boolean all=true;
+               for (HashSet set:punctuation)
+                     {
+                        if(!(set.contains(a)))
+                           {
+                              all = false;
+                              break;
+                           }
+                     }
+                     
+                if (all)
+                  {
+                     punctOrg.add((char)a);
+                  }
+           
+
+            }
+ 
+      System.out.println("Common lower case: ");
+      System.out.print(lowerOrg);
+      System.out.println("Common upper case: ");
+      System.out.print(upperOrg);
+      System.out.println("Common punctuation: ");
+      System.out.print(punctOrg);
+
+
+
+
+
+
+   }
+}
